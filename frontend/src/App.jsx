730cef322887c9ba8f6d254b6dd50a6bc45a584a
Kuldeep2047 <kuldeepgarg6993@gmail.com>
2025-10-14 23:24:34 +0530

    Add basic setup for resume builder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,29 @@
-// src/App.jsx
-import { Routes, Route, Link } from "react-router-dom";
-import SignupPage from "./pages/SignupPage";
-import LoginPage from "./pages/LoginPage";
-import ProfilePage from "./pages/ProfilePge";
-import RecommendationPage from "./pages/RecommendationPage";
-import LandingPage from "./pages/LandingPage";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Preview from './pages/Preview'
+import Login from './pages/Login'
 
-function App() {
+const App = () => {
   return (
-    <div>
-
+    <>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/recommendations" element={<RecommendationPage />} />
-        
+        <Route path='/' element={<Home  />}/>
+
+        <Route path='app' element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='builder/:resumeId' element={<ResumeBuilder />} />
+        </Route>
+
+        <Route path='view/:resumeId' element={<Preview  />}/>
+        <Route path='login' element={<Login  />}/>
+
       </Routes>
-    </div>
-  );
+    </>
+  )
 }
 
-
-
-
-
-
-export default App;
+export default App
