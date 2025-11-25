500249c6c0ae344a4860df8794617d3815e18f0a
Rinki4738 <rinkijha3315@gmail.com>
2025-11-25 22:50:05 +0530

    Removed upload resume card + cleaned Dashboard component

--- MCQ-genrator/mcq-app/src/pages/Result.jsx
+++
@@ -1,8 +0,0 @@
-export default function Result({ score, setStep }) {
-  return (
-    <div>
-      <h1>Your Score: {score}</h1>
-      <button onClick={() => setStep("home")}>Generate Again</button>
-    </div>
-  );
-}
